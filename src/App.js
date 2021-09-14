ebaa8b543639a9e98f31256b6c20fb7cae486c47
Saba Akhtar <sabasaeedakhtar@gmail.com>
2021-09-14 16:06:14 +1000

    updated package.json

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import AliceCarousel from 'react-alice-carousel';
+import "react-alice-carousel/lib/alice-carousel.css";
+import images from './utilities/sliderImages';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AliceCarousel autoPlay animationType={"fadeout"} autoPlayInterval={2000} infinite>
+      {images}
+      </AliceCarousel>
     </div>
   );
 }
 
 export default App;
+
+// git remote add origin https://github.com/Any22/React-Components.git
+// git branch -M main
+// git push -u origin main
+
